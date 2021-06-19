d36f5e44221bfb1188e9463cb8f86fcc043e77da
Abdullah <abdullahsh123@hotmail.com>
2021-06-19 05:07:02 +0500

    Initial commit

---
+++ app/src/main/java/com/project/ecomuikit/MainActivity.java
@@ -0,0 +1,21 @@
+package com.project.ecomuikit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+
+        setContentView(R.layout.activity_main);
+    }
+}
